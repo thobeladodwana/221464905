9815655338c067b22d362154e571cc146e24cd84
thobeladodwana <101553445+thobeladodwana@users.noreply.github.com>
2022-04-22 13:36:47 +0200

    Create CircleApp.java

---
+++ CircleApp.java
@@ -0,0 +1,18 @@
+package za.ac.wsu.s221464905; 
+
+	public class CircleApp{
+
+	public static void main(String[] args) {
+
+	 Circle object = new Circle(5.56,4,6);
+	 
+
+	 object.getX();
+	 object.getY();
+	 object.getRadius();
+	 object.calculateArea();
+	 object.calculateCircumference();
+	 object.calculateDiameter();
+
+	}
+}
